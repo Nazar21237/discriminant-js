056665c65c0a0878f9a1c5c722108d4c8c2351c5
Nazar <defleppard21237@gmail.com>
2021-03-27 14:35:20 +0200

    add function maxValue

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const firstNum = +prompt("Enter first number:") ;
+const secondNum = +prompt("Enter second number:") ;
+const number = +prompt("Enter number to check:")
+let msg = "Equal num"
+
+// 1) function Max
+
+const maxValue = function (val1, val2) {
+  if(isNaN(val1 - val2)) return null;
+  if (val1 > val2) return val1;
+  return val2;
+}
+console.log(maxValue(firstNum, secondNum));
