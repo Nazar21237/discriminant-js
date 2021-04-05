a28049f73937810ceb4b0f53ceb24c7619ea99b6
Nazar <defleppard21237@gmail.com>
2021-04-05 17:09:39 +0300

    add discriminant

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const firstNum = +prompt("Enter first number:") ;
 const secondNum = +prompt("Enter second number:") ;
-const number = +prompt("Enter number to check:")
+const number = +prompt("Enter number to check:");
+
 let msg = "Equal num"
 
 // 1) function Max
@@ -32,5 +33,38 @@ function checkEven(){
   console.log("Min value is:", minValue(firstNum, secondNum));
   if(checkEvenNumbers(num)) console.log(`${num} is even`)
   else console.log(`${num} is odd`);
+}
+
+// 4) discrim
+
+function solve(a,b,c){
+  if(!isNumber(a) || !isNumber(b) || !isNumber (c)){
+    console.log("Wrong input");
+    return null;
+  }
+  if (a === 0){
+    console.log("a === 0. It`s mistake!");
+    return null;
+  }
+  const D = discriminant(a,b,c);
+  console.log(`discriminant = ${D}`);
+  let result = 0;
+  if(D < 0){
+    console.log("No roots");
+    result = null;
+  }
+  else if(D === 0){
+    result = (-b + Math.sqrt(D)) / (2 * a);
+  }
+  else{
+    const x1 = (-b + Math.sqrt(D)) / (2 * a);
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
+    result = [x1, x2]
+  }
+  return result;
+
+}
+function discriminant (a,b,c){
+  return b ** 2 - (4 * a * c);
 }
 
